7c19262ad0ac26debcb53425965a029df136eedf
asenetcky <asenetcky@gmail.com>
2025-01-03 20:35:50 -0500

    init renv

---
+++ setup/initial/renv-init.R
@@ -0,0 +1,25 @@
+# setup renv the first time
+renv::init()
+
+# using pak because it plays better with Pop!_OS for me
+options(renv.config.pak.enabled = TRUE)
+
+# purrr first then walk the rest
+renv::install("purrr")
+
+c(
+  "dplyr",
+  "ggplot2",
+  "tidyr",
+  "vetiver",
+  "plumber",
+  "RSocrata",
+  "duckdb",
+  "nanoparquet",
+  "httr2",
+  "devtools"
+) |>
+  purrr::walk(
+    \(pkg) renv::install(pkg, prompt = FALSE)
+  )
+
